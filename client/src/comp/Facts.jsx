a7098af700b8b842443cbdf2cedf3243b3d21ff5
NOAH SMART <noahsmart48@gmail.com>
2024-08-15 16:04:05 +0100

    done2

--- client/src/comp/Facts.jsx
+++ client/src/comp/Facts.jsx
@@ -1,5 +1,15 @@
 export function FactsPage() {
-  const play = ["Game1", "Game1", "Game1", "Game1", "Game1", "Game1"];
-    
-  return <div></div>;
+  return (
+    <div>
+      <h2>Games To Play If You Are Bored</h2>
+      <h4>Mario Kart</h4>
+      <h4>Zelda</h4>
+      <h4>Fallout</h4>
+      <h4>Goat Simulator</h4>
+      <h4>Minecraft</h4>
+      <h4>GTA 5</h4>
+      <h4>F1 23</h4>
+      <h4>Fall Guys</h4>
+    </div>
+  );
 }
